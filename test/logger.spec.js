9e129352f3ceecd2c85fec2f640240bd4aa4d0bd
Anup Bishnoi <pixelsallover@gmail.com>
2015-05-04 01:03:10 -0400

    added moar tests!

--- test/logger.spec.js
+++ test/logger.spec.js
@@ -1,18 +1,45 @@
-var assert = require('assert');
+var assert = require('assert'),
+    sinon = require('sinon');
 var Logger = require('..');
 
 describe('Logger', function () {
-  it('provides a log function on host object', function () {
-    var obj = {};
-    Logger.call(obj, 'namespace');
-    assert('function' === typeof obj.log,
-      'Didn\'t provide obj.log()');
+  var obj;
+
+  beforeEach(function () {
+    sinon.stub(console, 'log');
+    obj = {};
+    Logger.call(obj);
   });
 
-  //describe('Logger.disable()', function () {
-    //it('disables all logging', function () {
+  afterEach(function () {
+    console.log.restore();
+  });
 
-    //});
-  //});
+  it('provides a log function on host object', function () {
+    assert('function' === typeof obj.log);
+  });
+
+  it('logs to console', function () {
+    assert(!console.log.called);
+    obj.log('log message');
+    assert(console.log.called);
+  });
+
+  describe('Logger.disable()', function () {
+    it('disables all logging', function () {
+      Logger.disable();
+      obj.log('log message');
+      assert(!console.log.called);
+    });
+  });
+
+  describe('Logger.enable()', function () {
+    it('enables all logging again', function () {
+      Logger.disable();
+      Logger.enable();
+      obj.log('log message');
+      assert(console.log.called);
+    });
+  });
 });
 
