982c783489e55850367650231aa9cf08bd3ed177
Anup Bishnoi <pixelsallover@gmail.com>
2015-11-07 14:43:06 -0500

    lesser colors, better formatting

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
 var colors = [
-    'lightseagreen', 'forestgreen', 'goldenrod', 'dodgerblue', 'darkorchid', 'crimson',
-    '#9BB291', '#5D8A79', '#3D4656', '#D7A954', '#8D714C',
-    '#B28272', '#CC76A1', '#DD9296', '#87B38D', '#828AB0', '#5B6195'
+    'lightseagreen', 'forestgreen', 'goldenrod', 'dodgerblue',
+    'darkorchid', 'crimson'
   ],
   colorMaps = {},
   colorIndex = 0,
   enabled = true;
 
 function Logger(context) {
-  colorMaps[context] = colorMaps[context] || colors[colorIndex++ % colors.length];
+  colorMaps[context] = colorMaps[context] ||
+    colors[colorIndex++ % colors.length];
   this.log = function () {
-    var colored = ['%c ' + context + ' ', 'color: white; background: ' + colorMaps[context] + ';'],
-      args = [].slice.call(arguments);
-    if (enabled) console.log.apply(console, colored.concat(args));
+    if (!enabled) {
+      return;
+    }
+    var colored = [
+      '%c ' + context + ' ',
+      'color: white; background: ' + colorMaps[context] + ';'
+    ];
+    var args = [].slice.call(arguments);
+    console.log.apply(console, colored.concat(args));
   };
 }
 
